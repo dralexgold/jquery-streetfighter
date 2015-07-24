b7bff390cbad0fcbb08ec2bcd23cd53bc3f7b2a5
Alex Gold <alex@alexgold.net>
2015-07-24 10:42:38 +1000

    jquery edit

--- js/app.js
+++ js/app.js
@@ -32,14 +32,11 @@ function playGame() {
 
   $(document).keydown(function(e) {
     if (e.keyCode == 88) {
-      playCool();
       $('.ryu-action').hide();
       $('.ryu-cool').show();
     }   
   }).keyup(function(e) {
     if (e.keyCode == 88) {
-      $('#cool')[0].pause();
-      $('#cool')[0].load();
       $('.ryu-cool').hide();
       $('.ryu-still').show();
     }
